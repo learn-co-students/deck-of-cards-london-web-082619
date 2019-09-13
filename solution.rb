5e917c2f94e1b1e02132931a12b65ac1f0794483
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-09-13 09:20:51 +0100

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,44 @@
+require 'pry'
+
 class Deck
+
+    @@ranks =  ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    @@suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+
+    attr_accessor :cards
+
+    def initialize #Deck.new
+        @@suits.each do |suit|
+            @@ranks.each do |rank|
+                card = Card.new(suit, rank, self)
+            end
+        end
+        @cards = Card.all.select{ |card| card.deck == self }
+    end
+
+    def choose_card 
+        card = @cards.delete_at(rand(0..@cards.length-1))
+    end
 
 end
 
 class Card
 
+    attr_reader :suit, :rank, :deck
+    @@all = []
+
+    def initialize(suit, rank, deck) #Card.new("Hearts", "Q")
+        @suit = suit
+        @rank = rank
+        @deck = deck
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
+
+binding.pry
+'save'
