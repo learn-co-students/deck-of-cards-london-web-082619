ae4120d3c949932ad774223cb416ebefb6044b73
Abdullah Bajaman <ambajaman@gmail.com>
2019-09-13 23:25:54 +0100

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,38 @@
-class Deck
+require 'pry'
 
+class Deck
+    attr_reader :cards
+    def initialize
+        @cards = []
+        rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        suit = ["Hearts", "Clubs", "Diamonds", "Spades"]
+        
+        suit.each{|s| 
+            rank.each{|s|
+                @cards << Card.new(suit, rank)
+                }
+        
+            }
+        # for suit in ["Hearts", "Clubs", "Diamonds", "Spades"] do
+        #     for rank in ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"] do
+        #         @cards << Card.new(suit, rank)
+        #         binding.pry
+        #     end
+        #   end
+    end
+
+    def choose_card
+        card_choosen = self.cards.sample
+        @cards.delete(card_choosen)
+    end
 end
 
 class Card
+    attr_reader :rank, :suit
+
+    def initialize(suit, rank)
+      @rank = rank
+      @suit = suit
+    end
 
 end
